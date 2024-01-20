7645f0ffc0f72b1ea4858620752b44d673dd5e21
Hayley Mahaney <hayley.mahaney@gmail.com>
2024-01-20 12:17:15 -0500

    Another vite config change.

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from "@vitejs/plugin-react";
 // Determine the base path based on the environment
 const base = process.env.NODE_ENV === "production" ? "/yearstocome/" : "/";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: base,
+  build: {
+    assetsDir: "assets",
+  },
 });
